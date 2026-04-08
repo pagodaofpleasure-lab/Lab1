bdc9a6e096aac44efaa2212b4c801da5557f2e3c
nekolover-study <Edkim2007irk@mail.ru>
2026-04-08 11:27:12 +0700

    added code for circle

---
+++ kim.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    double r, a;
+    const double PI = 3.1415926535;
+
+    cout << "Введите радиус и угол сектора: ";
+    cin >> r >> a;
+
+    cout << "Длина: " << 2 * PI * r << endl;
+    cout << "Площадь круга: " << PI * r * r << endl;
+    cout << "Площадь сектора: " << (PI * r * r * a) / 360.0 << endl;
+
+    return 0;
+}
